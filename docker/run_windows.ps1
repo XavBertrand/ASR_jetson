c2db35b71709ed97a29ff0902a6d1ba6e4f397cf
XavBertrand <bertrand.xav@gmail.com>
2025-10-17 18:10:00 +0200

    chore: remove obsolete files

--- docker/run_windows.ps1
+++
@@ -1,1 +0,0 @@
-docker run -it --rm --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -v C:\Users\bertr\PycharmProjects\ASR_Agent:/workspace -w /workspace xavier/asr-agent:dev-pc python scripts/run_asr_pipeline.py tests/data/test.mp3 --out outputs --lang fr --ns 1 --denoise
